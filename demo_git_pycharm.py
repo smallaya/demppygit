91ecb9c8d367ea27ed7571ee034ed62ee8f8a33e
Sudarsh <smallaya@gmail.com>
2023-01-06 07:18:01 +0530

    fixed spelling error

--- demo_git_pycharm.py
+++ demo_git_pycharm.py
@@ -15,6 +15,6 @@ def myfunction_multiply(x,y):
     return multipli
 
 add = myfunction_sum(10,20)
-multipli = myfunction_multipli(10,20)
+multipli = myfunction_multiply(10,20)
 print("Sum =",add)
 print("Multiply =",multipli)
