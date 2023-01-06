ef17fee41027180a7e1487e4fe1174db96a893a1
Sudarsh <smallaya@gmail.com>
2023-01-06 06:36:50 +0530

    updated demo code to add 2 numbers and display result

--- demo_git_pycharm.py
+++ demo_git_pycharm.py
@@ -1,5 +1,14 @@
 
 from datetime import date
+
 print ("Hello")
+
 today = date.today()
 print("Today's date:", today)
+
+def myfunction(x,y)
+    sum = x+y
+    return sum
+
+add = myfunction(10,20)
+print("Sum is %s",add)
