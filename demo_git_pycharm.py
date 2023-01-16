43d0d14a0ed43aabb7280f99201fc74941b18db8
sudarsh suresh mallaya <sudarsh.sureshmallaya@nxp.com>
2023-01-16 14:35:58 +0530

    Added sub div feature

--- demo_git_pycharm.py
+++ demo_git_pycharm.py
@@ -13,8 +13,25 @@ def myfunction_sum(x,y):
 def myfunction_multiply(x,y):
     multipli = x*y
     return multipli
+    
+    
+def myfunction_subtract(x,y):
+    subt = x-y
+    return subt
+
+def myfunction_Division(x,y):
+    divi = x/y
+    return divi
+
 
 add = myfunction_sum(10,20)
-multipli = myfunction_multiply(10,20)
 print("Sum =",add)
+
+multipli = myfunction_multiply(10,20)
 print("Multiply =",multipli)
+
+subtract = myfunction_subtract(10,20)
+print("Difference =",subtract)
+
+divide = myfunction_Division(10,20)
+print("Divide =",divide)
